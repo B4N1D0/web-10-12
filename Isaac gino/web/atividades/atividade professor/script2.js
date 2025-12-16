a8304b35c767ad7cc4dc2efafd5c582f47deb3e2
B4N1D0 <isaacgino6@gmail.com>
2025-12-15 22:11:43 -0300

    atividasdes do professor dia 15/12/2025

---
+++ Isaac gino/web/atividades/atividade professor/script2.js
@@ -0,0 +1,36 @@
+// let contagem = 10;
+
+// const intervalo = setInterval(function() {
+//     console.log('tempo restante')
+
+//     if (contagem === 0) {
+//         console.log('!FOGO');
+//         clearInterval(intervalo);
+//     }else{
+//         contagem--;
+//     }
+// } , 1000);
+    
+while(contador = 10){
+    if (contador === 0) {
+        console.log('!FOGO')
+    }else{
+        constador--;
+        console.log(contagem)
+    }
+}
+
+// const umNumero = +prompt('digite um numero aqui')
+// if (umNumero > 0){
+//     console.log('conseguiu carai')
+// }
+// if(umNumero < 0) {
+//     console.log('menor que zero serio')
+// }
+//  if(umNumero == 0){
+//     console.log('zero e chato escolhe outro')
+// }
+// if(umNumero %2 == 0) {
+//     console.log('seu numero e par')
+// }else{console.log('seu numero e impar')
+// }
